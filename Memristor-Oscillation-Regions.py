c038c00cdc6da4608f41a4d92f957c858303a58b
TedLuckins <tedluckins@gmail.com>
2025-03-03 10:13:55 +0000

    Update Memristor-Oscillation-Regions.py

--- Memristor-Oscillation-Regions.py
+++ Memristor-Oscillation-Regions.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 import time
 
-from Calculations import simulate_until_equilibrium, get_equilibrium_V, Precompute_CalcF
+from Calculations import simulate_until_equilibrium, get_equilibrium_V, Load_F_Values
+
 
 start_time = time.time()
 
@@ -22,7 +23,7 @@ def save_results_to_csv(Vn_values, Rn_values, V_results_min, V_results_max, file
 alpha = 1.0
 Omega = 7.0
 Gamma = 0.1
-Z_T = 0.3
+Z_T = 0.1
 Lambda = 0.13
 l = 0.5
 x_0 = 0.8
@@ -30,8 +31,7 @@ V_n = 1.0
 R_n = 5.0
 
 # Precompute values
-x_V_range = np.arange(-3, 3.01, 0.01)
-precomputed_F = Precompute_CalcF(x_V_range, l, Lambda)
+precomputed_F = Load_F_Values("Memristor/Conductance_Functions/Conductance_Functions.csv")
 
 Initial_Conditions_Memristor = [0, 0, 1.0, 0]
 
@@ -39,8 +39,8 @@ dt = 0.01
 max_steps = 100000
 
 # Define Vn and Rn ranges
-Vn_values = np.arange(0.01, 40.01, 1)
-Rn_values = np.arange(1, 11, 10)
+Vn_values = np.arange(0.1, 45.1, 0.1)
+Rn_values = np.arange(0.0, 205.0, 5.0)
 
 V_results_min = []
 V_results_max = []
@@ -54,11 +54,12 @@ current_initial_conditions = Initial_Conditions_Memristor.copy()
 # Sweeps over  Vn and Rn
 for R_n in Rn_values:
     for V_n in Vn_values:
-        results, current_initial_conditions = simulate_until_equilibrium(R_n, V_n, current_initial_conditions, max_steps, dt)
+        results, current_initial_conditions = simulate_until_equilibrium(V_n, R_n, current_initial_conditions, max_steps, dt, precomputed_F) #F0_0_dict, F0_1_dict, F1_1_dict)
         V_min, V_max = get_equilibrium_V(results)
 
         Time = time.time() - start_time
-
+        V_n = round(V_n, 1)
+        R_n = round(R_n, 0)
         Vn_list.append(V_n)
         Rn_list.append(R_n)
         V_results_min.append(V_min)
@@ -67,7 +68,7 @@ for R_n in Rn_values:
     print(f"Rn value {R_n} is complete at time {Time:.2f}")
 
 save_results_to_csv(Vn_list, Rn_list, V_results_min, V_results_max,
-                    "memristor_oscillation_regions_Test3.csv")
+                    "memristor_oscillation_regions_Test.csv")
 end_time = time.time()
 total_time = end_time - start_time
 print(f"Simulation completed in {total_time:.2f} seconds.")
