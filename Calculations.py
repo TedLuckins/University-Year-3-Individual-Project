c66b9a86face2aec24610712dae62a200b76bf10
TedLuckins <tedluckins@gmail.com>
2025-03-23 19:28:27 +0000

    Update Calculations.py

--- Calculations.py
+++ Calculations.py
@@ -5,9 +5,9 @@ from numba import njit
 from scipy.integrate import quad
 from scipy.misc import derivative
 from scipy.special import hermite, factorial
-import time
 
-start_time = time.time()
+
+
 
 
 # Numerical integration method functions
@@ -41,28 +41,30 @@ def Rossler_Deriv(state, a, b, c):
     Z_dot = b + Z * (X - c)
     return np.array([X_dot, Y_dot, Z_dot])
 
-def Memristor_Deriv(state, V_n, R_n, Z_T, alpha, Omega, Gamma, l, Lambda, x_0, precomputed_F): #F0_0_dict, F0_1_dict, F1_1_dict):
+def Memristor_Deriv(state, V_n, R_n, Z_T, alpha, Omega, Gamma, l, Lambda, x_0, precomputed_F):
     X, Y, Z, V = state
-    x_V = float(round(x_0 - l * np.sqrt(2) * V, 3))
-    Index = int((x_V - (-3))/0.001)
-    if Index < 0:
-        Index = 0
-    elif Index > 6000:
-        Index = 6000
+    x_V = round(x_0 - l * np.sqrt(2) * V, 3)
+    if x_V < -3:
+        F_vals = precomputed_F[-3]
+        F0_0 = F_vals[(0, 0)]
+        F0_1 = F_vals[(0, 1)]
+        F1_1 = F_vals[(1, 1)]
+    elif x_V > 3:
+        F_vals = precomputed_F[3]
+        F0_0 = F_vals[(0, 0)]
+        F0_1 = F_vals[(0, 1)]
+        F1_1 = F_vals[(1, 1)]
+    else:
+        # Interpolates
+        x_V1 = round(np.floor(x_V / 0.001) * 0.001, 3)
+        x_V2 = round(x_V1 + 0.001, 3)
+        F_vals1 = precomputed_F[x_V1]
+        F_vals2 = precomputed_F[x_V2]
+        F0_0 = ((F_vals2[(0, 0)]-F_vals1[(0, 0)])/0.001) * x_V
+        F0_1 = ((F_vals2[(0, 1)]-F_vals1[(0, 1)])/0.001) * x_V
+        F1_1 = ((F_vals2[(1, 1)]-F_vals1[(1, 1)])/0.001) * x_V
 
-    F_vals = precomputed_F[Index]
 
-
-
-    # F0_0 = F0_0_dict[Index]
-    # F0_1 = F0_1_dict[Index]
-    # F1_1 = F1_1_dict[Index]
-
-
-
-    F0_0 = F_vals[(0, 0)]
-    F0_1 = F_vals[(0, 1)]
-    F1_1 = F_vals[(1, 1)]
 
     V_dot = V_n - (1 + R_n * (F0_0 * (1 + Z) / 2 + F0_1 * X + F1_1 * (1 - Z) / 2)) * V
     X_dot = Omega * Y + 2 * V_dot * Z - Gamma * X
@@ -101,28 +103,13 @@ def Load_F_Values(filename):
     precomputed_F = {}
 
     for _, row in df.iterrows():
-        Index = int((row["x_V"] + 3)/0.001)
-        precomputed_F[Index] = {
+        #Index = int((row["x_V"] + 3)/0.001)
+        precomputed_F[row["x_V"]] = {
             (0, 0): row["F0_0"],
             (0, 1): row["F0_1"],
             (1, 1): row["F1_1"],
         }
     return precomputed_F
-
-# def Load_F_Values(filename):
-#     df = pd.read_csv(filename)
-#     F0_0_dict = {}
-#     F0_1_dict = {}
-#     F1_1_dict = {}
-#
-#     for _, row in df.iterrows():
-#         Index = (row["x_V"] + 3) / 0.001  # Compute the index position
-#         F0_0_dict[int(Index)] = row["F0_0"]
-#         F0_1_dict[int(Index)] = row["F0_1"]
-#         F1_1_dict[int(Index)] = row["F1_1"]
-#
-#     return F0_0_dict, F0_1_dict, F1_1_dict
-
 
 # Simulates a System across time
 def Simulate_System(Deriv_Func, Step_Func, Initial_Conditions, params, dt, num_steps, out_steps):
@@ -180,7 +167,6 @@ def Op_dt(Deriv_Func, Step_Func, Initial_Conditions, params, max_dt, num_steps, 
     return dt
 
 
-
 # Dictionary
 Calculations = {
     "Euler": Euler_Method,
@@ -190,21 +176,21 @@ Calculations = {
     "Memristor": Memristor_Deriv
 }
 def has_reached_equilibrium(V_values, threshold=1e-7):
-    last_10_percent = V_values[int(len(V_values) * 0.99):]  # Last 10% of time steps
+    last_10_percent = V_values[int(len(V_values) * 0.99):]
     if (np.max(last_10_percent) - np.min(last_10_percent)) < threshold:
         return True
     else:
         return False
 
 
-def simulate_until_equilibrium(V_n, R_n, initial_conditions, max_steps, dt, precomputed_F): #F0_0_dict, F0_1_dict, F1_1_dict):
+def simulate_until_equilibrium(V_n, R_n, Z_T, alpha, Omega, Gamma, l, Lambda, x_0, initial_conditions, max_steps, dt, precomputed_F):
     steps_per_check = 1000
     state = np.array(initial_conditions)
     results = []
 
     for step in range(1, max_steps + 1):
         state = Runge_Kutta_Method(Memristor_Deriv, state, dt, V_n, R_n, Z_T, alpha, Omega, Gamma, l, Lambda, x_0,
-                                   precomputed_F) #F0_0_dict, F0_1_dict, F1_1_dict)
+                                   precomputed_F)
         results.append([step * dt] + list(state))
 
         if step % steps_per_check == 0:
@@ -218,14 +204,22 @@ def simulate_until_equilibrium(V_n, R_n, initial_conditions, max_steps, dt, prec
 
 def get_equilibrium_V(results):
     V_values = results[:, -1]
-    last_10_percent = V_values[int(len(V_values) * 0.99):]  # Last 10% of data
+    last_10_percent = V_values[int(len(V_values) * 0.99):]
 
-    if (np.max(last_10_percent) - np.min(last_10_percent)) < 1e-7:
+    if (np.max(last_10_percent) - np.min(last_10_percent)) < 1e-8:
         return last_10_percent[-1], last_10_percent[-1]
     else:
         return np.min(last_10_percent), np.max(last_10_percent)
 
 
+def save_results_to_csv(Vn_values, V_results_min, V_results_max, filename):
+    df = pd.DataFrame({
+        "Vn": Vn_values,
+        "V_min": V_results_min,
+        "V_max": V_results_max
+    })
+    df.to_csv(filename, index=False)
+    print(f"Results saved to {filename}")
 
 def save_results_to_csv(Vn_values, Rn_values, V_results_min, V_results_max, filename):
     df = pd.DataFrame({
@@ -236,86 +230,3 @@ def save_results_to_csv(Vn_values, Rn_values, V_results_min, V_results_max, file
     })
     df.to_csv(filename, index=False)
     print(f"Results saved to {filename}")
-
-# Parameters for Memristor System
-alpha = 1.0
-Omega = 7.0
-Gamma = 0.1
-Z_T = 1.0
-Lambda = 0.13
-l = 0.5
-x_0 = 0.8
-V_n = 1.0
-R_n = 5.0
-
-# # Precompute values
-# x_V_range = np.arange(-3, 3.01, 0.01)
-# precomputed_F = Precompute_CalcF(x_V_range, l, Lambda)
-#F0_0_dict, F0_1_dict, F1_1_dict = Load_F_Values("Memristor/Conductance_Functions/Conductance_Functions.csv")
-
-precomputed_F = Load_F_Values("Memristor/Conductance_Functions/Conductance_Functions.csv")
-
-Initial_Conditions_Memristor = [0, 0, 1.0, 0]
-
-current_initial_conditions = Initial_Conditions_Memristor.copy()
-
-dt = 0.01
-max_steps = 100000
-
-# # Simulates and saves memristor system with Runge-Kutta method
-# results_memristor = Simulate_System(
-#     Step_Func=Runge_Kutta_Method,
-#     Deriv_Func=lambda state, *params: Memristor_Deriv(state, *params, ),
-#     Initial_Conditions=Initial_Conditions_Memristor,
-#     params=(V_n, R_n, Z_T, alpha, Omega, Gamma, l, Lambda, x_0),
-#     dt=0.01,
-#     num_steps=100000,
-#     out_steps=100000
-# )
-# Save_Results(results_memristor, "memristor_simulation_TestN.csv", "Memristor")
-
-# results_memristor, state = simulate_until_equilibrium(
-#     V_n, R_n, current_initial_conditions, max_steps, dt, F0_0_dict, F0_1_dict, F1_1_dict)
-
-# results_memristor, state = simulate_until_equilibrium(
-#     V_n, R_n, current_initial_conditions, max_steps, dt, precomputed_F)
-#
-# Save_Results(results_memristor, "memristor_simulation_TestN.csv", "Memristor")
-
-# # Conductance Function Simulation for each quantum state
-# x_V_Range = np.arange(-3, 3.01, 0.001)
-# Lambda = 0.13
-# l = 0.5
-# results_conductance_function = []
-# for x_V in x_V_Range:
-#     x_V = round(x_V, 4)
-#     F0_0 = Calc_F(0, 0, x_V = x_V, l = l, Lambda = Lambda)
-#     F0_1 = Calc_F(0, 1, x_V = x_V, l = l, Lambda = Lambda)
-#     F1_1 = Calc_F(1, 1, x_V = x_V, l = l, Lambda = Lambda)
-#     results_conductance_function.append([x_V, F0_0, F0_1, F1_1])
-#     Time = time.time() - start_time
-#     print(f"Current time {Time}")
-#
-# results_conductance_function = np.array(results_conductance_function)
-# Save_Results(results_conductance_function, "Memristor/Conductance_Functions/Conductance_Functions.csv", "Conductance Functions")
-
-
-
-
-# Save for memristor equalibrium V
-def save_results_to_csv(Vn_values, V_results_min, V_results_max, filename):
-    df = pd.DataFrame({
-        "Vn": Vn_values,
-        "V_min": V_results_min,
-        "V_max": V_results_max
-    })
-    df.to_csv(filename, index=False)
-    print(f"Results saved to {filename}")
-
-
-
-
-
-end_time = time.time()
-total_time = end_time - start_time
-print(f"Simulation completed in {total_time:.2f} seconds.")
